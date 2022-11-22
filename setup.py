8a82898e640cdc4a82cb53ba60c261fdabc259ad
Tyler Jones <tyler.jones@logmein.com>
2022-11-22 13:31:51 -0700

    add pyprojecttoml

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
+__version__ = "0.0.0"
+
 setup(
     name="multi-search",
-    version="0.0.1",
+    version=__version__,
     author="tyler jones",
     description="Simple multiple search source plugin style wrapper.",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/tylerhjones/multi-search/",
     packages=find_packages(),
-    use_scm_version=True,
-    setup_requires=['setuptools_scm'],
     install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3.10",
